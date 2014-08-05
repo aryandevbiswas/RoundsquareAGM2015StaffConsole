893bb557c97b49355d9b708d68aacc4f7f267386
aryandevbiswas <aryandevbiswas@gmail.com>
2014-08-05 22:53:31 +0400

    Restored original Version 1.1
    I've restored the original version 1.1, bugs will be fixed shortly
    
    Note that The Images and Install Creater Application files are also now
    in the git

--- windows-chrome/start.bat
+++ windows-chrome/start.bat
@@ -1,8 +1,8 @@
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::Built By Aryan Biswas (http://aryanbiswasgroup.co.nf/)	::
 ::Copyright 2014 Indian High School (http://ihsdxb.com/)	::
 ::Roundsquare AGM 2015 Staff Console Ver 1.1 (Windows)		::
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo on
 goto START
 :START
