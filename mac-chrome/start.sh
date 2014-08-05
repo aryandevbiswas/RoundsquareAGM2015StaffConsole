5054c57abb789b8696a4edc426e20fcbd27cd365
Aryan Biswas <aryandevbiswas@gmail.com>
2014-08-05 16:57:59 +0400

    Create start.sh

---
+++ mac-chrome/start.sh
@@ -0,0 +1,6 @@
+#########################################################
+#Built By Aryan Biswas (http://aryanbiswasgroup.co.nf/)	#
+#Copyright 2014 Indian High School (http://ihsdxb.com/)	#
+#Roundsquare AGM 2015 Staff Console Ver 1.1(OSX,Chrome)	#
+#########################################################
+open -a "Google Chrome" --args --app=http://aryanbiswasapi.co.nf/RSIHS/staff/webapp/
