c55f6e5b02518f93e00451362432b370225f4dab
Aryan Biswas <aryandevbiswas@gmail.com>
2014-08-13 17:22:26 +0400

    This will now launch the new webapp

--- windows-internetexplorer/start.bat
+++ windows-internetexplorer/start.bat
@@ -1,14 +1,14 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::Built By Aryan Biswas (http://aryanbiswasgroup.co.nf/)	::
 ::Copyright 2014 Indian High School (http://ihsdxb.com/)	::
-::Roundsquare AGM 2015 Staff Console Ver 1.1 (Windows)		::
+::Roundsquare AGM 2015 Staff Console Ver 1.2 (Windows)		::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo on
 goto START
 :START
 goto LAUNCH
 :LAUNCH 
-start iexplore.exe -k http://aryanbiswasapi.co.nf/RSIHS/staff/webapp/
+start iexplore.exe -k http://ihsroundsquare.net/staff/webapp/
 tasklist /FI "IMAGENAME eq iexplore.exe" 2>NUL | find /I /N "iexplore.exe">NUL
 if "%ERRORLEVEL%"=="0" goto EXIT 
 :EXIT
