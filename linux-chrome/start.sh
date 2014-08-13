d2cafc4c31b0e3ac0bde55f151233851400064e7
Aryan Biswas <aryandevbiswas@gmail.com>
2014-08-13 17:25:52 +0400

    This will now launch the new webapp

--- linux-chrome/start.sh
+++ linux-chrome/start.sh
@@ -1,6 +1,6 @@
 #################################################################
 #Built By Aryan Biswas (http://aryanbiswasgroup.co.nf/)		#
 #Copyright 2014 Indian High School (http://ihsdxb.com/)		#
-#Roundsquare AGM 2015 Staff Console Ver 1.1 (Linux,Chrome)	#
+#Roundsquare AGM 2015 Staff Console Ver 1.2 (Linux,Chrome)	#
 #################################################################
-google-chrome --app=http://aryanbiswasapi.co.nf/RSIHS/staff/webapp
+google-chrome --app=http://ihsroundsquare.net/staff/webapp
