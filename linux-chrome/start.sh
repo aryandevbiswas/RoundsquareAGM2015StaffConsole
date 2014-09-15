1a705fc6bc93d742cd420626e5fd9cb5a4c2699d
Aryan Biswas <aryandevbiswas@gmail.com>
2014-09-15 23:39:04 +0400

    Start Script 1.3-beta
    Start Script for 1.3-beta

--- linux-chrome/start.sh
+++ linux-chrome/start.sh
@@ -1,6 +1,6 @@
 #################################################################
 #Built By Aryan Biswas (http://aryanbiswasgroup.co.nf/)		#
 #Copyright 2014 Indian High School (http://ihsdxb.com/)		#
-#Roundsquare AGM 2015 Staff Console Ver 1.2 (Linux,Chrome)	#
+#Round Square AGM 2015 Staff Console Ver 1.2 (Linux,Chrome)	#
 #################################################################
-google-chrome --app=http://ihsroundsquare.net/staff/webapp
+google-chrome --app=http://staff.ihsroundsquare.net/webapp/?version=1.3-beta
